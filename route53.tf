d1ca187b9d4b0654d81f1ae9558a22ebfd568165
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-07 21:14:15 +0000

    add

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
   zone_id = "Z1DFF4Z8NNX6OO"
-  name    = ".wordpress.umar.mobi"
+  name    = "wordpress.umar.mobi"
   type    = "A"
   ttl     = "60"
   records = ["${aws_instance.web.public_ip}"]
