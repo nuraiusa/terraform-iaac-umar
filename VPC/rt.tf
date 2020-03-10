440e3eb119d76d90948aff88d47c6a6a13ff7102
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-10 23:42:55 +0000

    add

--- VPC/rt.tf
+++ VPC/rt.tf
@@ -3,10 +3,7 @@ resource "aws_route_table" "dev" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.dev.id}"
-}
-  tags {
-    Environment = "${var.Environment}${count.index +1 }"
-    Create_by = "${var.Created_by}"
+
   }
 }
 
