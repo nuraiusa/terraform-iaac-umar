d9d07670fd9284c173f0d0000d9b778fec21b3ec
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-11 21:27:13 +0000

    add

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-class-umar"
-key = "tower/us-east-1/tools/Honk Kong/tower.tfstate"
+key = "tower/us-east-1/tools/Mumbai/tower.tfstate"
 region = "us-east-1"
   }
 }
