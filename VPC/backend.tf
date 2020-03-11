5ca0cbf0322e020a60842625f13b515f3df768b9
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-11 02:05:04 +0000

    add

--- VPC/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-backend "s3" {
-bucket = "terraform-class-umar"
-key = "tower/us-east-1/tools/oregion/tower.tfstate"
-region = "us-east-1"
-  }
-}
--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-class-umar"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-    #dynamodb_table = "terraform-class20"
-  }
-}
