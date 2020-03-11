e28834f0bcac9f1061fe09fbdec932d7cd94187f
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-11 00:41:49 +0000

    add

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -16,19 +16,19 @@ resource "aws_subnet" "dev3" {
 
 #Private Subenets
 resource "aws_subnet" "dev_private1" {
-  vpc_id = "${aws_vpc.dev.id}"
+  vpc_id = "${aws_vpc.dev_private1.id}"
   cidr_block = "${var.cidr_block1_private}"
 
 }
 
 resource "aws_subnet" "dev_private2" {
-  vpc_id = "${aws_vpc.dev.id}"
+  vpc_id = "${aws_vpc.dev_private2.id}"
   cidr_block = "${var.cidr_block2_private}"
 
 }
 
 resource "aws_subnet" "dev_private3" {
-  vpc_id = "${aws_vpc.dev.id}"
+  vpc_id = "${aws_vpc.dev_private3.id}"
   cidr_block = "${var.cidr_block3_private}"
 
 }
