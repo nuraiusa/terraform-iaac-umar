7f9ef42c1c81c92932fcc18c5d385085509ed82d
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-11 21:08:21 +0000

    add

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,8 +1,7 @@
-erraform {
-  backend "s3" {
-    bucket = "terraform-class-umar"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-    #dynamodb_table = "terraform-class20"
+terraform {
+backend "s3" {
+bucket = "terraform-class-umar"
+key = "tower/us-east-1/tools/Virgina/tower.tfstate"
+region = "us-east-1"
   }
 }
--- backend.tf
+++ backend.tf
@@ -1,7 +1,10 @@
 terraform {
-backend "s3" {
-bucket = "terraform-class-umar"
-key = "tower/us-east-1/tools/Virgina/tower.tfstate"
-region = "us-east-1"
-  }
-}
+    backend "s3" {
+        bucket = "terraform-class-umar"
+        key = "terraform/terraform/tfvars"
+        region = "us-east-1"
+        #dynamodb_table = "terraform-class"
+      }
+    }
+
+    
