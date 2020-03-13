09121e71af28aa4fc8c9b69240801ef9ab5c040a
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-13 00:32:48 +0000

    add

---
+++ VPC/output.tf
@@ -0,0 +1,32 @@
+outuput "VPC" {
+   value   = "${module.dev.VPC}"
+}
+
+outuput "Subnet1" {
+   value   = "${module.dev.Subnet1}"
+}
+
+
+outuput "Subnet2" {
+   value   = "${module.dev.Subnet2}"
+}
+
+outuput "Subnet3" {
+   value   = "${module.dev.Subnet2}"
+}
+
+outuput "Private_Subent1" {
+   value   = "${module.dev.Private_Subent1}"
+}
+
+outuput "Private_Subent2" {
+   value   = "${module.dev.Private_Subnet2}"
+}
+
+outuput "Private_Subent3" {
+   value   = "${module.dev.Private_Subent3}"
+}
+
+outuput "IGW" {
+   value   = "${module.dev.IGW}"
+}
