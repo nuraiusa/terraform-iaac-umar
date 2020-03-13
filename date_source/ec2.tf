f48cf4865f1cd543f1fad9b3fd1ca6d605e3cb66
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-13 00:39:42 +0000

    ss

--- date_source/ec2.tf
+++ date_source/ec2.tf
@@ -56,7 +56,7 @@ resource "aws_key_pair" "provisioner" {
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
-
+  key_name      = "${aws_key_pair.provisioner.key_name}"
   tags = {
     Name = "HelloWorld"
   }
