5175b483cbd39f809f4f355a6a09d21ea3976793
Cloud User <centos@ip-172-31-19-40.eu-west-2.compute.internal>
2020-03-13 00:18:44 +0000

    add

--- date_source/ec2.tf
+++ date_source/ec2.tf
@@ -47,3 +47,13 @@ data "aws_ami" "centos" {
 output "CENTOS_AMI_ID" {
   value   ="${data.aws_ami.centos.id}"
   }
+
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
